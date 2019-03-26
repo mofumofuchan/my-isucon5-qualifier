553a7cb115de3d408791583457c9cf45cbd11c8f
AYASE Eisui <a_eisui@yahoo.co.jp>
2019-03-26 17:44:18 +0900

    Change unicorn config to make log files

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -2,3 +2,6 @@ worker_processes 1
 preload_app true
 listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
+stderr_path '/home/isucon/webapp/ruby/unicorn_stderr.log'
+stdout_path '/home/isucon/webapp/ruby/unicorn_stdout.log'
+
